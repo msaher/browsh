f5d8caf0c48e84afc67e9fe255bb74e46109d8e3
saher <msaher.shair@gmail.com>
2026-02-17 14:27:43 +0300

    Incremental commit for src/ui/web/src/hooks/cmp.ts line 1

--- src/ui/web/src/hooks/cmp.ts
+++ src/ui/web/src/hooks/cmp.ts
@@ -1,13 +1,1 @@
 import * as config from "../config"
-import { createSignal, type Signal } from "solid-js";
-
-export function addCompletion(input: HTMLInputElement, item: string) {
-  let start = 0;
-  let end = 0;
-  const cursorPos = input.selectionStart ?? 0;
-  // insert
-  if (cursorPos === 0) {
-    input.value = item
-    // return [0, 0] as const
-    return
-  }
