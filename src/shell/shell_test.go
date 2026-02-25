15ca0a79dd3266ac6818f030605dd8cb6b029e73
saher <msaher.shair@gmail.com>
2026-02-25 09:52:49 +0300

    Incremental commit for src/shell/shell_test.go line 1

--- src/shell/shell_test.go
+++ src/shell/shell_test.go
@@ -1,13 +1,1 @@
 package shell
-
-import (
-	"testing"
-	"fmt"
-)
-
-func tokenTypes(tokens []Token) []TokenType {
-	types := make([]TokenType, len(tokens))
-	for i, t := range tokens {
-		types[i] = t.Type
-	}
-	return types
